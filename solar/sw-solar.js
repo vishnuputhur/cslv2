12322bcc84adc68a3d977ee4d3f2abc35d513454
vishnuputhur <vishnuputhur@yahoo.com>
2026-01-25 16:27:50 +0530

    Create sw-solar.js

---
+++ solar/sw-solar.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'solar-v2';
+const ASSETS = ['index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
